0cc80a367b556451a1a3d1b7b7f2f5f3c4386d1c
onlyhappiness96 <onlyhappiness96@gmail.com>
2021-11-21 18:45:40 +0900

    Create MainPage

---
+++ app/src/components/Map.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react'
+
+const { kakao } = window;
+
+const Location=()=>{
+
+  useEffect(()=>{
+    var container = document.getElementById('map');
+    var options = {
+      center: new kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
+      level: 3
+    };
+    var map = new kakao.maps.Map(container, options);
+    }, [])
+
+
+    return (
+        <div>
+          <div 
+            id="map" 
+            style={{width:"100%", height:"100vh"}}>
+          </div> 
+        </div>
+    )
+}
+
+export default Location;
