01c34c4e794ac849b97d093535c741d57df9db84
onlyhappiness96 <onlyhappiness96@gmail.com>
2021-11-21 13:22:06 +0900

    Update Sidebar

--- sandbox/layout/src/App.js
+++ sandbox/layout/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Sidebar from './components/Sidebar';
+import Map from './components/Map';
+
+import './App.css'
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  height: '100vh'
+}));
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Box sx={{ flexGrow: 1 }} >
+      <Grid container spacing={1.5} >
+        <Grid item xs={3} >
+          <Item><Sidebar /></Item>
+        </Grid>
+        <Grid item xs={9}>
+          <Item><Map /></Item>
+        </Grid>
+        {/* <Grid item xs={4}>
+          <Item>xs=4</Item>
+        </Grid>
+        <Grid item xs={8}>
+          <Item>xs=8</Item>
+        </Grid> */}
+      </Grid>
+    </Box>
+  )
 }
 
-export default App;
+export default App
---
+++ sandbox/layout/src/components/Map.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Map() {
+  return (
+    <div>
+      맵 입니다!
+    </div>
+  )
+}
+
+export default Map
