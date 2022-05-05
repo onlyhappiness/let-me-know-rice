a4784eabbbc38a1659eeb7e4e156a9ebff82853b
onlyhappiness <onlyhappiness96@gmail.com>
2022-05-05 22:34:47 +0900

    Progess..

--- App/src/main.js
+++ App/src/main.js
@@ -1,26 +1,26 @@
-const { app, BrowserWindow } = require('electron') ;
+const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-function createWindow () { 
-  const win = new BrowserWindow({ 
-    width: 1280, 
-    height: 800, 
-    webPreferences: { 
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
       nodeIntegration: true,
-      contextIsolation : false,
-    } 
-  }) 
-  win.loadURL("http://localhost:3000/")
-} 
-app.whenReady().then(() => { 
-  createWindow() 
+      contextIsolation: false,
+    },
+  });
+  win.loadURL('http://localhost:3000');
+}
+app.whenReady().then(() => {
+  createWindow();
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-}) 
-app.on('window-all-closed', () =>  { 
-  if (process.platform !== 'darwin') app.quit() 
-})
+  // app.on('activate', () => {
+  //   if (BrowserWindow.getAllWindows().length === 0) {
+  //     createWindow();
+  //   }
+  // });
+});
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
