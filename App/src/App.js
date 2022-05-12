2371fce0bbcd8f0f15de03c314b11ee5dc10c597
onlyhappiness <onlyhappiness96@gmail.com>
2022-05-12 11:30:10 +0900

    [ Progress ] Login page

--- App/src/App.js
+++ App/src/App.js
@@ -1,17 +1,29 @@
 import "./App.css";
 
-import { BrowserRouter } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
 import { useState } from "react";
 
 import DashboardLayout from "./components/dashboard/dashboard-layout";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         {/* <DashboardLayout /> */}
-        <Login />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route
+            path="/home"
+            element={<Home />}
+          />
+        </Routes>
+        {/* <Login /> */}
       </div>
     </BrowserRouter>
   );
--- App/src/pages/Login.jsx
+++ App/src/pages/Login.jsx
@@ -1,119 +1,79 @@
 import React, {
-  useEffect,
+  // useEffect,
   useState,
 } from "react";
 
 import "antd/dist/antd.css";
-import {
-  Form,
-  Input,
-  Button,
-  Checkbox,
-} from "antd";
+// import {
+//   Form,
+//   Input,
+//   Button,
+//   Checkbox,
+// } from "antd";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigate = useNavigate();
+
   // 유저 id
-  const [userId, onChangeUserId] = useState("");
+  const [Email, setEmail] = useState("");
+  const onEmailHandler = (e) => {
+    // console.log(e.target.value);
+    setEmail(e.target.value);
+  };
 
   // 유저 비밀번호
-  const [userPassword, onChangeUserPassword] =
-    useState("");
-
-  let navigate = useNavigate();
-
-  const onFinish = (values) => {
-    console.log("Success:", values);
+  const [Password, setPassword] = useState("");
+  const onPasswordHandler = (e) => {
+    setPassword(e.target.value);
   };
 
-  const onFinishFailed = (errorInfo) => {
-    console.log("Failed:", errorInfo);
-  };
+  // TODO: Submit
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
 
-  // console.log(userId);
-  // console.log(userPassword);
+    let user = {
+      email: Email,
+      password: Password,
+    };
+
+    console.log(user);
+
+    if (
+      user.email === "test1@gmail.com" &&
+      user.password === "test1"
+    ) {
+      navigate("/home");
+    }
+  };
 
   return (
-    <Form
-      name="basic"
+    <form
       style={{
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
-        alignContent: "center",
+        alignItems: "center",
+        width: "100%",
       }}
-      // labelCol={{
-      //   span: 8,
-      // }}
-      // wrapperCol={{
-      //   span: 16,
-      // }}
-      // initialValues={{
-      //   remember: true,
-      // }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      // autoComplete="off"
+      onSubmit={onSubmitHandler}
     >
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[
-          {
-            required: true,
-            message:
-              "Please input your username!",
-          },
-        ]}
-      >
-        <Input
-          value={userId}
-          onChange={onChangeUserId}
-        />
-      </Form.Item>
+      <label>Email</label>
+      <input
+        type={"email"}
+        value={Email}
+        onChange={onEmailHandler}
+      />
 
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message:
-              "Please input your password!",
-          },
-        ]}
-      >
-        <Input.Password
-          value={userPassword}
-          onChange={onChangeUserPassword}
-        />
-      </Form.Item>
+      <label>Password</label>
+      <input
+        type={"password"}
+        value={Password}
+        onChange={onPasswordHandler}
+      />
 
-      <Form.Item
-        name="remember"
-        valuePropName="checked"
-        wrapperCol={{
-          offset: 8,
-          span: 16,
-        }}
-      >
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
-
-      <Form.Item
-        wrapperCol={{
-          offset: 8,
-          span: 16,
-        }}
-      >
-        <Button
-          type="primary"
-          // htmlType="submit"
-        >
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+      <button type="submit">Login</button>
+    </form>
   );
 }
 
