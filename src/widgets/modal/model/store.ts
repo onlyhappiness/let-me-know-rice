6f063ac55ebda83ae3ca8a748474d42772a7bac7
onlyhappiness <onlyhappiness96@gmail.com>
2024-09-26 23:23:57 +0900

    add: shadcn/ui

---
+++ src/widgets/modal/model/store.ts
@@ -0,0 +1,16 @@
+// import { atom } from "jotai";
+
+// export const modalsAtom = atom([]);
+
+// export const addModalAtom = atom(null, (get, set, modal) => {
+//   const modals = get(modalsAtom);
+//   set(modalsAtom, [...modals, modal]);
+// });
+
+// export const removeModalAtom = atom(null, (get, set, modal) => {
+//   const modals = get(modalsAtom);
+//   set(
+//     modalsAtom,
+//     modals.filter((m) => m !== modal)
+//   );
+// });
